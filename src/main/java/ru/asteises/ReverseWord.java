91922d7c122577076de0cdf220090a9c9d370746
Asteises <p.nikitaev@gmail.com>
2022-12-23 17:02:55 +0300

    Добавил Анаграмму, Перевернуть слово и доделал Прогноз погоды...

--- src/main/java/ru/asteises/ReverseWord.java
+++ src/main/java/ru/asteises/ReverseWord.java
@@ -1,5 +1,17 @@
 package main.java.ru.asteises;
 
+import java.util.*;
+
 // Дана стринга, необходимо ее развернуть;
 public class ReverseWord {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите слово:");
+        String word = scanner.nextLine();
+
+        StringBuilder stringBuilder = new StringBuilder(word);
+        System.out.println("Перевернули слово: " + stringBuilder.reverse());
+    }
 }
--- src/main/java/ru/asteises/WeatherForecastStack.java
+++ src/main/java/ru/asteises/WeatherForecastStack.java
@@ -1,5 +1,6 @@
 package main.java.ru.asteises;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class WeatherForecastStack {
@@ -17,18 +18,23 @@ public class WeatherForecastStack {
 
         Stack<Pair> stack = new Stack<>();
         int[] num = {13, 12, 15, 11, 9, 12, 16};
+        int[] result = new int[num.length]; // Создаем новый массив такой же длины как и первый;
+        //Ожидаем результат: [2, 1, 4, 2, 1, 1, 0]
 
-        for (int i = num.length - 1; i >= 0; i--) {
-            Pair pair = new Pair(i, num[i]);
-            stack.push(pair);
-            // складываем значение в новый массив
+        for (int i = num.length - 1; i >= 0; i--) { // проходимся по массиву с конца;
+            Pair pair = new Pair(num[i], i); // каждый раз создаем новую пару;
+            if (stack.empty()) { // Чтобы положить первый элемент в стек;
+                stack.push(pair);
+                continue;
+            }
+            if (pair.value > stack.peek().value) {
+                stack.pop();
+            }
+            result[pair.index] = stack.peek().index - pair.index;
+            if (stack.size() == 1) {
+                stack.push(pair);
+            }
         }
+        System.out.println(Arrays.toString(result));
     }
-
-    // stack.pop() вытаскиваем из стека с удалением
-    // stack.peek() вытаскиваем без удаления
-    // stack.push() кладём в стек
-    // pair 5 12
-    // pair 6 16
-
 }
