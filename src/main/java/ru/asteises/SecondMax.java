6509b85bceb29ab3f2b429b3416aaf22c884b6e7
Asteises <p.nikitaev@gmail.com>
2022-12-23 17:20:00 +0300

    Добавил Анаграмму, Перевернуть слово и доделал Прогноз погоды...

--- src/main/java/ru/asteises/SecondMax.java
+++ src/main/java/ru/asteises/SecondMax.java
@@ -1,5 +1,20 @@
 package main.java.ru.asteises;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 // Реализовать алгоритм поиска индекса второго максимального значения в массиве;
 public class SecondMax {
+
+    public static void main(String[] args) {
+
+        int[] num = {13, 12, 15, 11, 9, 12, 16};
+        List<Integer> list = Arrays.stream(num).boxed().toList();
+        Integer max = Collections.max(list);
+        list.remove(max);
+        int secondMax = Collections.max(list);
+        System.out.println(secondMax);
+    }
+
 }
