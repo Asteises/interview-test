914231523cb43ebed3c9cd3e20f6a884b11cd324
Asteises <p.nikitaev@gmail.com>
2023-01-12 16:01:47 +0300

    something new

---
+++ src/main/java/ru/asteises/SeparateMethod.java
@@ -0,0 +1,22 @@
+package main.java.ru.asteises;
+
+public class SeparateMethod {
+
+    public static void main(String[] args) {
+        System.out.println(classicSolution("Дом", "И", 6));
+    }
+
+    public static String classicSolution(String line, String separator, int count) {
+
+        StringBuilder sb = new StringBuilder();
+        if (count <= 0) {
+            return line;
+        } else {
+            for (int i = 0; i < count; i++) {
+                sb.append(line).append(separator);
+            }
+            sb.delete(sb.length() - separator.length(), sb.length());
+        }
+        return String.valueOf(sb);
+    }
+}
---
+++ src/main/java/ru/asteises/SumVowels.java
@@ -0,0 +1,20 @@
+package main.java.ru.asteises;
+
+public class SumVowels {
+
+    public static void main(String[] args) {
+        System.out.println(classicSolution("айболит"));
+    }
+
+    public static int classicSolution(String word) {
+        String vowels = "аеёийоуыэюя";
+        char[] chars = word.toCharArray();
+        int result = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (vowels.indexOf(chars[i]) != -1) {
+                result++;
+            }
+        }
+        return result;
+    }
+}
