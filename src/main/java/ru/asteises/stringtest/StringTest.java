2619b3631b4c44f998ab6acbd1144e6ed8f9f4ca
Asteises <p.nikitaev@gmail.com>
2023-01-29 18:17:39 +0300

    test conflicts

--- src/main/java/ru/asteises/stringtest/StringTest.java
+++ src/main/java/ru/asteises/stringtest/StringTest.java
@@ -3,10 +3,10 @@ package main.java.ru.asteises.stringtest;
 public class StringTest {
 
     public static void main(String[] args) {
-        String s3 = "qwe";
+        String s4 = "qwzxczxczxczxcce";
         String s2 = "string 1";
-        s3 = s3 + s2;
+        s4 = s4 + s2;
 
-        System.out.println(s3);
+        System.out.println(s4);
     }
 }
