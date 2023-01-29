143d63aff43ed555ff4a69f0a9fa66ea96096528
Asteises <p.nikitaev@gmail.com>
2023-01-29 18:16:00 +0300

    test conflicts

---
+++ src/main/java/ru/asteises/stringtest/StringTest.java
@@ -0,0 +1,12 @@
+package main.java.ru.asteises.stringtest;
+
+public class StringTest {
+
+    public static void main(String[] args) {
+        String s3 = "qwe";
+        String s2 = "string 1";
+        s3 = s3 + s2;
+
+        System.out.println(s3);
+    }
+}
